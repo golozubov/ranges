a36d210d5c55b76002e2079769efb59b39385f61
artem_golozubov <golozubov@gmail.com>
2019-02-23 12:04:14 +0300

    Initial

---
+++ src/RangeCollection.js
@@ -0,0 +1,32 @@
+// Task: Implement a 'Range Collection' class.
+// A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
+// A range collection is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
+
+/**
+ * RangeCollection class
+ * NOTE: Feel free to add any extra member variables/functions you like.
+ */
+export class RangeCollection {
+    /**
+     * Adds a range to the collection
+     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     */
+    add(range) {
+        // TODO: implement this
+    }
+
+    /**
+     * Removes a range from the collection
+     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     */
+    remove(range) {
+        // TODO: implement this
+    }
+
+    /**
+     * Prints out the list of ranges in the range collection
+     */
+    print() {
+        // TODO: implement this
+    }
+}
